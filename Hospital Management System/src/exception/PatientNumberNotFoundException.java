62647a5fdc5bf8fd400ad657ff28b1d5cd6fb403
Makizhna <134484163+Makizhna@users.noreply.github.com>
2025-04-09 14:58:22 +0530

    Hospital Management System

--- Hospital Management System/src/exception/PatientNumberNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package exception;
-
-
-// Custom exception when patient is not found
-public class PatientNumberNotFoundException extends Exception {
-    public PatientNumberNotFoundException(String message) {
-        super(message);
-    }
-}
