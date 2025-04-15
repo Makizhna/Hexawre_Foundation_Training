3639f6433346c9b226230367a5708ffdf8a14b21
Makizhna <134484163+Makizhna@users.noreply.github.com>
2025-04-15 10:00:23 +0530

    Update ICarLeaseRepository.java

--- Case Study/Car Rental System/src/dao/ICarLeaseRepository.java
+++ Case Study/Car Rental System/src/dao/ICarLeaseRepository.java
@@ -3,11 +3,13 @@ package dao;
 import entity.Vehicle;
 import exception.CarNotFoundException;
 import exception.CustomerNotFoundException;
+import exception.DataNotFoundException;
 import exception.LeaseNotFoundException;
 import entity.Customer;
 import entity.Lease;
 import entity.Payment;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ICarLeaseRepository {
@@ -16,27 +18,28 @@ public interface ICarLeaseRepository {
     void removeCar(int carID) throws CarNotFoundException; // Removes a car by ID
     List<Vehicle> listAvailableCars();                   // Lists all available cars
     List<Vehicle> listRentedCars();                      // Lists all rented cars
-    Vehicle findCarById(int carID) throws CarNotFoundException; // Finds a car by ID
+    Vehicle findCarById(int carId) throws DataNotFoundException;          // Finds a car by ID
+
 
     // Customer Management
     void addCustomer(Customer customer);                // Adds a new customer
     void removeCustomer(int customerID) throws CustomerNotFoundException; // Removes a customer by ID
     List<Customer> listCustomers();                     // Lists all customers
-    Customer findCustomerById(int customerID);          // Finds a customer by ID
+    Customer findCustomerById(int customerID) throws CustomerNotFoundException; // Finds a customer by ID
 
     // Lease Management
     Lease createLease(int customerID, int carID, java.sql.Date startDate, java.sql.Date endDate); // Creates a new lease
     void returnCar(int leaseID) throws LeaseNotFoundException; // Returns a car based on lease ID
     List<Lease> listActiveLeases();                    // Lists all active leases
     List<Lease> listLeaseHistory();                   // Lists all lease history
-
-    // Payment Handling
-    void makePayment(Payment payment);                // Records a payment for a lease
-    List<Payment> listPayments();                     // Retrieves all payments
-    Payment findPaymentById(int paymentID);           // Finds a payment by ID
-    void recordPayment(Lease lease, double amount);
-
-  
-
+    Lease getLeaseById(int leaseId) throws DataNotFoundException;        //get lease by id
     
+    
+    // Payment Handling
+    void makePayment(Payment payment);                // make  payment for a lease
+    List<Payment> listPayments();                     // Retrieves all payments
+    Payment findPaymentById(int paymentID) throws DataNotFoundException;      // Finds a payment by ID
+    void recordPayment(Lease lease, double amount);         // records payment for a lease
+	
+
 }
